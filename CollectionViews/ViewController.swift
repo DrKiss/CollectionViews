da42dd006d5418a951efd8634ac93adaad9c4c5b
Robert <robert.covu@gmail.com>
2021-10-13 22:30:58 -0400

    CollectionViews

--- CollectionViews/ViewController.swift
+++ CollectionViews/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let reuseIdentifier = "cellIdentifier"
 
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        collectionView.dataSource = self
+        
+        
+        self.view.addSubview(collectionView)
+        
     }
 
+    //Collectionview methods
+     
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         100
+     }
+     
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as UICollectionViewCell
+         cell.backgroundColor = self.randomColor()
+         
+         return cell
+     }
+     
+     func randomColor() -> UIColor {
+         let red = CGFloat(drand48())
+         let green = CGFloat(drand48())
+         let blue = CGFloat(drand48())
+         
+         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+     }
 
 }
 
